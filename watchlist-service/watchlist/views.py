3e6610408224c999325e66af8c6ca241e059d846
Mastwal Mesfin <126337103+ENDragnee@users.noreply.github.com>
2026-01-21 11:13:12 +0300

    feat: watchlist intergration with k8s

--- watchlist-service/watchlist/views.py
+++ watchlist-service/watchlist/views.py
@@ -1,6 +1,4 @@
-from rest_framework import viewsets, status
-from rest_framework.response import Response
-from rest_framework.decorators import action
+from rest_framework import viewsets
 from .models import Watchlist
 from .serializers import WatchlistSerializer
 from .permissions import IsOwner
@@ -17,13 +15,13 @@ class WatchlistViewSet(viewsets.ModelViewSet):
     permission_classes = [IsOwner]
 
     def get_queryset(self):
-        user_id = getattr(self.request.user, 'id', None)
+        user_id = getattr(self.request.user, "id", None)
         if not user_id:
             return Watchlist.objects.none()
-        return Watchlist.objects.filter(user_id=str(user_id)).order_by('-created_at')
+        return Watchlist.objects.filter(user_id=str(user_id)).order_by("-created_at")
 
     def perform_create(self, serializer):
-        user_id = getattr(self.request.user, 'id', None)
+        user_id = getattr(self.request.user, "id", None)
         serializer.save(user_id=str(user_id))
 
     def retrieve(self, request, *args, **kwargs):
@@ -32,8 +30,8 @@ class WatchlistViewSet(viewsets.ModelViewSet):
 
     def update(self, request, *args, **kwargs):
         # Allow updating the status (and other safe fields), but ensure user can't change owner
-        if 'user_id' in request.data:
-            request.data.pop('user_id')
+        if "user_id" in request.data:
+            request.data.pop("user_id")
         return super().update(request, *args, **kwargs)
 
     def destroy(self, request, *args, **kwargs):
--- watchlist-service/watchlist_service/settings.py
+++ watchlist-service/watchlist_service/settings.py
@@ -1,74 +1,94 @@
 import os
 from pathlib import Path
 import dj_database_url
+from corsheaders.defaults import default_headers
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'unsafe-dev-secret')
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "unsafe-dev-secret")
 
-DEBUG = os.environ.get('DEBUG', 'False').lower() in ('1', 'true', 'yes')
+DEBUG = os.environ.get("DEBUG", "False").lower() in ("1", "true", "yes")
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
 
 INSTALLED_APPS = [
-    'django.contrib.contenttypes',
-    'django.contrib.auth',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'watchlist',
+    "django.contrib.contenttypes",
+    "django.contrib.auth",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "corsheaders",
+    "rest_framework",
+    "watchlist",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
 ]
 
-ROOT_URLCONF = 'watchlist_service.urls'
+CORS_ALLOWED_ORIGINS = [
+    "http://dracula.com",
+    "http://auth.dracula.com",
+    "http://localhost:3000",
+]
+
+
+CORS_ALLOW_HEADERS = list(default_headers) + [
+    "content-disposition",
+]
+
+ROOT_URLCONF = "watchlist_service.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'watchlist_service.wsgi.application'
+WSGI_APPLICATION = "watchlist_service.wsgi.application"
 
-DATABASE_URL = os.environ.get('DATABASE_URL', f'sqlite:///{BASE_DIR / "db.sqlite3"}')
-DATABASES = {
-    'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
-}
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.environ.get("POSTGRES_DB", "ds_movie_watchlist"),
+#         "USER": os.environ.get("POSTGRES_USER", "admin"),
+#         "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "qazwsxedc"),
+#         "HOST": os.environ.get("POSTGRES_HOST", "postgres-service-watchlist"),
+#         "PORT": "5432",
+#     }
+# }
+
+DATABASE_URL = os.environ.get("DATABASE_URL", f'sqlite:///{BASE_DIR / "db.sqlite3"}')
+DATABASES = {"default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)}
 
 AUTH_PASSWORD_VALIDATORS = []
 
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
 USE_I18N = False
 USE_L10N = False
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'watchlist.authentication.JWTAuthentication',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
+    "DEFAULT_AUTHENTICATION_CLASSES": ("watchlist.authentication.JWTAuthentication",),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
 
 # JWT secret used by the authentication class - provided via environment
-JWT_SECRET = os.environ.get('JWT_SECRET', 'replace-me')
+JWT_SECRET = os.environ.get("JWT_SECRET", "replace-me")
